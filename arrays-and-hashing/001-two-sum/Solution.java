567bc57227faa68f92e947e50b91f3cb67704617
Luka Petricevic <luka.petricevic@smp-studio.de>
2026-03-05 10:17:30 +0100

    Clean up

--- arrays-and-hashing/001-two-sum/Solution.java
+++ arrays-and-hashing/001-two-sum/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
-class Solution {
+class TwoSumSolution {
     public static void main(String[] args) {
         int[] nums = {2,7, 11, 15};
         int target = 9;
---
+++ src/arrays_and_hashing/valid_anagram/Solution.java
@@ -0,0 +1,4 @@
+package arrays_and_hashing.valid_anagram;
+
+public class Solution {
+}
