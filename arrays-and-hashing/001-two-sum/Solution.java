bb2d9a466e2cea85126b79d48e5424100af6778d
Luka Petricevic <luka.petricevic@smp-studio.de>
2026-03-05 09:39:04 +0100

    Clean repo structure

--- arrays-and-hashing/001-two-sum/Solution.java
+++ arrays-and-hashing/001-two-sum/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Solution {
+class Solution {
     public static void main(String[] args) {
         int[] nums = {2,7, 11, 15};
         int target = 9;
