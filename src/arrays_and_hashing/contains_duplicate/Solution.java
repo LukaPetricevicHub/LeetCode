a6d7219a6fea708f628b90d80ad2a75d6ad129a2
Luka Petricevic <luka.petricevic@smp-studio.de>
2026-03-09 11:24:49 +0100

    Set up contains duplicate

---
+++ src/arrays_and_hashing/contains_duplicate/Solution.java
@@ -0,0 +1,18 @@
+package arrays_and_hashing.contains_duplicate;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1, 2, 3, 1};
+        boolean result = solution.containsDuplicate(nums);
+
+        System.out.println(result);
+    }
+
+
+    public boolean containsDuplicate(int[] nums) {
+        return false;
+    }
+}
